caa381a8f2e1657b7c4485676d73e2c11f1cb2d2
lauramcalpine <lauramcalpine@localhost>
2024-10-09 14:45:37 -0500

    flag!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,13 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(255,0,255);
+		StdDraw.filledRectangle(0.5, 0.5, 0.6, 0.30);
+		StdDraw.setPenColor(0,0,128);
+		StdDraw.filledCircle(0.5, 0.5, 0.2);
+
+		
+		
 		
 	}
 }
